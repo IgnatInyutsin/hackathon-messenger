10f28a83a8ec36aa044126b71b7a14663c98408e
IgnatInyutsin <IgnatInyutsin>
2023-03-22 07:44:25 +0400

    sidebar reg/log fixed

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
   public showMenu= true;
   name1:string="Egor";
   avatar1:string = "user.png";
   name2:string="Ingnar";
   avatar2:string = "user.png";
+  regLogBlockable: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+    if (window.location.pathname == "/login" || window.location.pathname == "/signup") this.regLogBlockable = true;
+  }
 }
