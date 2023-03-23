9842525d35fb02535fb6b9074363b95dc76e1884
IgnatInyutsin <IgnatInyutsin>
2023-03-23 14:02:44 +0400

    добавил регистрацию, добавил редирект незарегистрированным

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import {HttpClient} from "@angular/common/http";
+import {Connector} from "./restapi";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService) {}
+
+  ngOnInit(): void {
+    //проверяем, существует ли сессия. Если нет - редирект
+    if (this.cookieService.get("token") == '' && window.location.pathname != "/login" && window.location.pathname != "/signup") {
+      location.replace("/login")
+    }
+  }
 }
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Connector} from "../restapi";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // для ngModel
+  loginForm: any = {
+    username: "",
+    password: ""
+  }
+  // сообщения об ошибках
+  errors: any = {
+    emptyNickname: false,
+    emptyPassword: false,
+    incorrectData: false,
   }
 
+  constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService) { }
+
+  ngOnInit(): void {}
+
+  login(): void {
+    //очищение экрана от любых ошибок
+    this.errors.emptyNickname = false;
+    this.errors.emptyPassword = false;
+
+    // Валидация
+    if (this.loginForm.username == "") {
+      this.errors.emptyNickname = true;
+      return;
+    }
+    if (this.loginForm.password == "") {
+      this.errors.emptyPassword = true;
+      return;
+    }
+
+    this.http.post(this.connector.url + "api/auth/token/login/", this.loginForm).
+    subscribe((data: any) => {
+      // если получилось - добавляем в cookie токен сессии и перезагружаем страницу
+      this.cookieService.set("token", data.auth_token)
+      location.reload()
+    }, (error) => {
+      // иначе - сообщение об ошибке
+      this.errors.incorrectData = true;
+      // очищаем поля
+      this.loginForm.password = ""
+      this.loginForm.username = ""
+    })
+  }
 }
