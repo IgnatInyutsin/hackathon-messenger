10634c9cfb9b6dadc2f251e53e569fa0ce320faa
IgnatInyutsin <IgnatInyutsin>
2023-03-23 19:29:58 +0400

    Исправил chat на chats в URL (Так правильней по правилам построения REST)

--- backend/api/restapi/app/routes.py
+++ backend/api/restapi/app/routes.py
@@ -5,7 +5,7 @@ from restapi.app.views import *
 #устанавливаем пути
 router = routers.DefaultRouter()
 router.register("user/search", UserSearchViewSet)
-router.register("chat", ChatViewSet)
+router.register("chats", ChatViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
