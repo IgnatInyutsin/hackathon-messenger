2daddfc949525d7a84360134cd8e513481ef4fe0
IgnatInyutsin <IgnatInyutsin>
2023-03-23 14:07:37 +0400

    проверка действительности токена

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import {HttpClient} from "@angular/common/http";
-import {Connector} from "./restapi";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Connector } from "./restapi";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +17,19 @@ export class AppComponent {
     //проверяем, существует ли сессия. Если нет - редирект
     if (this.cookieService.get("token") == '' && window.location.pathname != "/login" && window.location.pathname != "/signup") {
       location.replace("/login")
+    }
+    else { //проверяем, действителен ли токен
+      this.http.get(this.connector.url + "api/auth/users/me/", {
+        headers: new HttpHeaders({
+          "Authorization": "Token " + this.cookieService.get("token") // добавляем заголовок авторизации
+        })
+      }).subscribe(() => {
+        //если существует - ничего не делаем
+      }, () => {
+        //если не существует - разлогиниваемся
+        this.cookieService.delete("token");
+        location.reload();
+      });
     }
   }
 }
