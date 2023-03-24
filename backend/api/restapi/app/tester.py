7d503f1823d4d837a1f7ba9b8bcd9b4d52ae387b
ishao <70522007+IgnatInyutsin@users.noreply.github.com>
2023-03-24 11:41:57 +0400

    Delete tester.py

--- backend/api/restapi/app/tester.py
+++
@@ -1,5 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://localhost:98/ws/chat/4/?token=161ef9b129a1ac755bb7d55ff28e730c58808c32")
-ws.send("")
-result =  ws.recv()
-print(result)
