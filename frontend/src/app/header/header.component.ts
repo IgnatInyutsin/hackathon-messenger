b3a1798b468024f39eff0a0e1a117004ba654648
Егор Кулишов <naladwepo@gmail.com>
2023-03-25 22:23:37 +0400

    Реализовал поиск по имени в создании чата

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -10,19 +10,54 @@ import {CookieService} from "ngx-cookie-service";
 })
 
 export class HeaderComponent implements OnInit {
+  userSearchForm: any = {
+    username: "",
+  }
+  errors: any = {
+    emptyNickname: false,
+  }
   public showMenu= true;
   name1:string="Egor";
   avatar1:string = "user.png";
   name2:string="Ingnat";
   avatar2:string = "user.png";
   regLogBlockable: boolean = false;
+  newData: Array<string> =[];
+  test:boolean = false;
+
   constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService) { }
 
   ngOnInit(): void {
     if (window.location.pathname == "/login" || window.location.pathname == "/signup") this.regLogBlockable = true;
+    this.http.get(this.connector.url + "api/user/search/", {params: {username: ""}}).
+    subscribe((data: any) => {
+      // здесь уже все перебираем
+      this.newData = data.results.map((num: any) => num.username);
+    }, (error) => {
+      // очищаем поля
+      this.userSearchForm.username = ""
+    })
   }
   goOut(): void{
     this.cookieService.delete('token');
     location.reload()
   }
+  userSearch(): void{
+    //очищение экрана от любых ошибок
+    this.errors.emptyNickname = false;
+
+    // Валидация
+    if (this.userSearchForm.username == "") {
+      this.errors.emptyNickname = true;
+      return;
+    }
+    this.http.get(this.connector.url + "api/user/search/", {params: {username: this.userSearchForm.username}}).
+    subscribe((data: any) => {
+      // здесь уже все перебираем
+      this.newData = data.results.map((num: any) => num.username);
+    }, (error) => {
+      // очищаем поля
+      this.userSearchForm.username = ""
+    })
+}
 }
