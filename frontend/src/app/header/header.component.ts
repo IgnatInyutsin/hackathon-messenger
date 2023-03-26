c5fb7e1a38bb5cb47066d24ab1650ce3bae49684
Егор Кулишов <naladwepo@gmail.com>
2023-03-26 20:22:18 +0400

    Сделал создание чата, но без фикса на бд

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Connector} from "../restapi";
 import {CookieService} from "ngx-cookie-service";
 
@@ -11,11 +11,14 @@ import {CookieService} from "ngx-cookie-service";
 })
 
 export class HeaderComponent implements OnInit {
+
   userSearchForm: any = {
     username: "",
+    chatName: "",
   }
   errors: any = {
     emptyNickname: false,
+    emptyChatName: false,
   }
   public showMenu= true;
   name1:string="Egor";
@@ -27,21 +30,25 @@ export class HeaderComponent implements OnInit {
   dataButtons: Array<boolean> = [];
   test:boolean = false;
   dataObj: { [key: string]: boolean } = {};
+  newDataObj: {[username: string]: [boolean, number]} = {};
+  trueIds: Array<number> = [];
+  trueIdsObjArr: Array<object> = [];
 
   constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService) { }
 
   ngOnInit(): void {
     if (window.location.pathname == "/login" || window.location.pathname == "/signup") this.regLogBlockable = true;
-    this.http.get(this.connector.url + "api/user/search/", {params: {username: ""}}).
+    this.http.get(this.connector.url + "api/user/search/", {params: {username: this.userSearchForm.username}, headers:new HttpHeaders({"Authorization": "Token " + this.cookieService.get("token")})}).
     subscribe((data: any) => {
       // здесь уже все перебираем
       this.newData = data.results.map((num: any) => num.username);
-      for (let i = 0; i<this.newData.length;i++){
-        this.dataButtons.push(false);
-      };
       for (const key of this.newData) {
         this.dataObj[key] = false;
       }
+      this.newDataObj = data.results.reduce((obj: {[username: string]: [boolean, number]}, {id, username}: {id: number, username: string}) => {
+        obj[username] = [false, id];
+        return obj;
+      }, {});
     }, (error) => {
       // очищаем поля
       this.userSearchForm.username = ""
@@ -60,16 +67,53 @@ export class HeaderComponent implements OnInit {
       this.errors.emptyNickname = true;
       return;
     }
-    this.http.get(this.connector.url + "api/user/search/", {params: {username: this.userSearchForm.username}}).
+    this.http.get(this.connector.url + "api/user/search/", {params: {username: this.userSearchForm.username}, headers:new HttpHeaders({"Authorization": "Token " + this.cookieService.get("token")})}).
     subscribe((data: any) => {
       // здесь уже все перебираем
       this.newData = data.results.map((num: any) => num.username);
       for (const key of this.newData) {
         this.dataObj[key] = false;
       }
+      this.newDataObj = data.results.reduce((obj: {[username: string]: [boolean, number]}, {id, username}: {id: number, username: string}) => {
+        obj[username] = [false, id];
+        return obj;
+      }, {});
     }, (error) => {
       // очищаем поля
       this.userSearchForm.username = ""
     })
 }
+  createChat(): void{
+    this.errors.emptyChatName = false;
+    if (this.userSearchForm.emptyChatName == "") {
+      this.errors.emptyChatName = true;
+      return;
+    }
+    for(const key in this.newDataObj){
+      let value: any = this.newDataObj[key]
+      if (value[0] === true){
+        this.trueIds.push(value[1])
+      }
+    }
+    this.trueIdsObjArr = this.trueIds.map(id => ({ id }));
+    this.http.post(this.connector.url + "api/chats/", {name:this.userSearchForm.chatName, type:"gm", members: this.trueIdsObjArr}, {headers: new HttpHeaders({"Authorization": "Token " + this.cookieService.get("token")})}).
+    subscribe((data: any) => {
+      console.log(data)
+
+    }, (error) => {
+      // иначе - сообщение об ошибке
+      this.userSearchForm.username = "";
+      this.userSearchForm.chatName = ""
+    })
+  }
+  getMineID():void{
+  this.http.get(this.connector.url + "api/auth/users/me/", {headers:new HttpHeaders({"Authorization": "Token " + this.cookieService.get("token")})}).
+  subscribe((data: any) => {
+  return data;
+}, (error) => {
+  // иначе - сообщение об ошибке
+
+})
+  }
+
 }
