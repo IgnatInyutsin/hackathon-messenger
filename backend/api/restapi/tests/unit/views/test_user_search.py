c03e6c75147d74f0217926a3a07c020edab467c1
IgnatInyutsin <IgnatInyutsin>
2023-03-26 08:29:31 +0400

    unit refactoring

--- backend/api/restapi/tests/unit/views/test_user_search.py
+++ backend/api/restapi/tests/unit/views/test_user_search.py
@@ -42,32 +42,3 @@ class UserSearchViewSetTestCase(TestCase):
 
         self.request.query_params["username"] = "TOLya"
         self.assertEqual(len(UserSearchViewSet.get_queryset(self)), 2)
-
-class ChatViewSetTestCase(TestCase):
-    def setUp(self):
-        # Создаем чаты
-        self.chats = [Chat(name="test",type="pm"),
-                Chat(name="test2", type="pm"),
-                Chat(name="test3", type="gm")]
-
-        # Создаем пользователя
-        self.request = Request()
-        self.request.user = User(id=1,
-                                 username="test",
-                                 password="test")
-        self.request.user.save()
-
-
-        # Сохраняем в бд
-        for chat in self.chats:
-            chat.save()
-            chat.members.add(self.request.user)
-            chat.save()
-
-        # пустой чат
-        Chat(name="test4", type="gm").save()
-
-
-    def test_get_queryset_list(self):
-        self.action = "list"
-        self.assertEqual(len(ChatViewSet.get_queryset(self)), len(self.chats))
