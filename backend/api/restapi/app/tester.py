c2bbd862218dc072633aa57472b5f4138286e7fa
IgnatInyutsin <IgnatInyutsin>
2023-03-27 19:58:54 +0400

    finally

--- backend/api/restapi/app/tester.py
+++ backend/api/restapi/app/tester.py
@@ -1,5 +1,5 @@
 import websocket
 ws = websocket.WebSocket()
 ws.connect("ws://localhost:98/ws/chat/7/?token=161ef9b129a1ac755bb7d55ff28e730c58808c32")
-ws.send('{"text": "ты чурка"}')
+ws.send('{"text": "хуйrr"}')
 print(ws.recv())
--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -11,22 +11,31 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class ChatComponent implements AfterViewInit {
   @ViewChild('chatblock', {static: false}) chatBlock:any;
+  messageFormText: string = ""
   chatData: any = {
     messages: []
   }
   myID: number;
-
+  ws: WebSocket;
   constructor(private http: HttpClient, private connector: Connector, public cookieService: CookieService, private activateRoute: ActivatedRoute) { }
 
   ngAfterViewInit(): void {
-    // скролл вниз чата
-    this.chatBlock.nativeElement.scrollTop = this.chatBlock.nativeElement.scrollHeight;
     // узнаем историю сообщений
     this.http.get(this.connector.url + "api/chats/" + this.activateRoute.snapshot.params['id'] + "/",
       {headers: new HttpHeaders({"Authorization": "Token " + this.cookieService.get("token")})})
       .subscribe((data) => {
         this.chatData = data;
         console.log(data)
+        // подключаем вебсокет
+        this.ws = new WebSocket(this.connector.wsUrl + "ws/chat/" + this.activateRoute.snapshot.params['id'] + "/?token=" + this.cookieService.get("token"))
+        this.ws.onmessage = (data) => {
+          this.getNewMessage(data.data);
+          // скролл вниз чата
+          setInterval(this.chatBlock.nativeElement.scrollTop = this.chatBlock.nativeElement.scrollHeight, 200);
+        }
+        // скролл вниз чата
+        // скролл вниз чата
+        setInterval(this.chatBlock.nativeElement.scrollTop = this.chatBlock.nativeElement.scrollHeight, 200);
       });
     // узнаем свой ID
     this.http.get(this.connector.url + "api/auth/users/me/",
@@ -34,6 +43,26 @@ export class ChatComponent implements AfterViewInit {
       .subscribe((data: any) => {
         this.myID = data.id;
       })
+  }
+
+  getNewMessage(data: any): void {
+    data = JSON.parse(data);
+    // добавляем сообщение на экран
+    this.chatData.messages.push({
+      "text": data["text"],
+      "author": {
+        username: data["author_name"],
+        id: data["author_id"]
+      }
+    });
+    this.chatBlock.nativeElement.scroll.top = this.chatBlock.nativeElement.scrollHeight
+  }
+
+  sendMessage(): void {
+    if (this.messageFormText != "") {
+      this.ws.send('{"text": "' + this.messageFormText + '"}')
+      this.messageFormText = "";
+    }
   }
 
 }
